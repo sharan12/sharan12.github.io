7e42544ccaf5175cd6eb4169924bfd678b4e9171
sharanshinduja <sharanshinduja@gmail.com>
2019-07-21 14:06:56 +0530

    1.8
    1.8

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(morgan('combined')); 
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+ 
+});
+
+
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+  console.log(`Server listening on port ${port}!`);
+});
